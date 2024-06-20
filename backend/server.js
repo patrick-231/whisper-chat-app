a0b2ec6d9f0333237142ce6819af3b90d226aa19
patrick-231 <137486318+patrick-231@users.noreply.github.com>
2024-06-20 12:19:08 +0200

    Implement backend server structure and models

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,12 @@ import colors from "colors";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-//files import
+//files import/
+import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -14,16 +17,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
-app.use(express.json()); // to parse the incoming request with JSON payloads
-
 //middleware
-app.use("/api/auth", authRoutes);
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser()); // to parse the incoming cookies from req.cookies
 
 //routes
 app.get("/", (req, res) => {
   res.send("Firing from Whisper Chat-App Backend");
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongDB();
