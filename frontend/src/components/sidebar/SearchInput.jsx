eda8235a286d236a548324c1dcb37fc1d5d3d58d
patrick-231 <137486318+patrick-231@users.noreply.github.com>
2024-06-22 19:19:24 +0200

    Implement Tailwind CSS and add new components and pages

---
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -0,0 +1,17 @@
+import { CiSearch } from "react-icons/ci";
+
+const SearchInput = () => {
+  return (
+    <form className="flex items-center gap-2">
+      <input
+        type="text"
+        placeholder="Search…"
+        className="input input-bordered rounded-full"
+      />
+      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+        <CiSearch className="w-6 h-6 outline-none" />
+      </button>
+    </form>
+  );
+};
+export default SearchInput;
