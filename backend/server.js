7845b7efa8b067a5ad203f0020736ba59fd7e486
patrick-231 <137486318+patrick-231@users.noreply.github.com>
2024-06-27 21:21:59 +0200

    Modifications to the main.jsx file in the frontend

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,10 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT, () => {
   connectToMongDB();
   console.log(`Server is listening on http://localhost:${PORT}`.cyan);
